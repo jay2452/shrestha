53110312735609c39e2649294ac67c90e641bcda
jay2452 <jay2452@live.com>
2016-10-28 10:45:25 +0530

    mail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
     RestClient.post "https://api:#{ENV['MAILGUN_API_SHRESTHA']}"\
    "@api.mailgun.net/v3/shresthasolutions.com/messages",
    :from => "Shrestha Site <mailgun@shresthasolutions.com>",
-   :to => "nihar_ic@yahoo.com",
+   :to => "jay2452@live.com, jayantt@hotmail.com, jayant2452@gmail.com",
    :subject => "Contact Enquirey",
    :text => "Name: #{params["name"]}\nEmail : #{params[:email]} \n #{params["comments"]}"
   end
