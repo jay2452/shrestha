6e740d9694400976198eb69fcb26d6b7b283a862
jay2452 <jay2452@live.com>
2016-10-28 10:42:55 +0530

    mailgun added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def send_mail
+    RestClient.post "https://api:#{ENV['MAILGUN_API_SHRESTHA']}"\
+   "@api.mailgun.net/v3/shresthasolutions.com/messages",
+   :from => "Shrestha Site <mailgun@shresthasolutions.com>",
+   :to => "nihar_ic@yahoo.com",
+   :subject => "Contact Enquirey",
+   :text => "Name: #{params["name"]}\nEmail : #{params[:email]} \n #{params["comments"]}"
+  end
 end
