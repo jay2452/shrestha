8e026735c6b92973987ec6b7f52f0d46347d86b3
jay2452 <jay2452@live.com>
2016-10-28 11:53:13 +0530

    sdsd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,12 @@ class WelcomeController < ApplicationController
   end
 
   def send_mail
-    RestClient.post "https://api:#{ENV['MAILGUN_API_SHRESTHA']}"\
+   puts RestClient.post("https://api:#{ENV['MAILGUN_API_SHRESTHA']}"\
    "@api.mailgun.net/v3/shresthasolutions.com/messages",
    :from => "Shrestha Site <mailgun@shresthasolutions.com>",
    :to => "jay2452@live.com, jayantt@hotmail.com, jayant2452@gmail.com",
    :subject => "Contact Enquirey",
-   :text => "Name: #{params["name"]}\nEmail : #{params[:email]} \n #{params["comments"]}"
+   :text => "Name: #{params["name"]}\nEmail : #{params[:email]} \n #{params["comments"]}")
 
    redirect_to :back, notice: "Sent !!, we will be contacting you shortly"
   end
